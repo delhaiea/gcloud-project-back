6a4b51a155dfbd1a049699bc0b611cdc2f66d91f
Alexis Delhaie <alexis.delhaie@yahoo.com>
2020-01-24 11:54:45 +0100

    cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const comments = require('./routes/comments');
 const films = require('./routes/film');
@@ -11,8 +12,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
+app.use(cors());
 
-app.use('/api/comment', comments);
-app.use('/api/film', films);
+app.use('/api/comment', cors(), comments);
+app.use('/api/film', cors(), films);
 
 module.exports = app;
